f1443365cf7057f9dfd97c4f19aebc49e749d5da
LoZo <m.czernik12@gmail.com>
2023-08-29 23:21:24 +0200

    last commit

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -10,7 +10,7 @@ export const ChannelCard = ({ channelDetail }) => {
   return (
     <Link
       to={
-        location.pathname === "/"
+        location.pathname === "/lozo-yt-site"
           ? `/channel/${channelDetail?.id?.channelId}`
           : ""
       }
